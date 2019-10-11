9435b384e3f27ec1a5cfe2d43a76d770bb6789cd
muramidaza <muramidaza@gmail.com>
2019-10-11 20:30:41 +0300

    start 2

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = ['name', 'address', 'website', 'email'];
+
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ const routes = [
 	{
 	   path: '/',
 	   components: {
-	       ompaniesIndex: CompaniesIndex
+	       companiesIndex: CompaniesIndex
 	   }  
 	},
 	{path: '/admin/companies/create', component: CompaniesCreate, name: 'createCompany'},
