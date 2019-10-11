0796df7f298a99897572713c5615e214edcc7d98
muramidaza <muramidaza@gmail.com>
2019-10-11 20:44:18 +0300

    start 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+	
+    public function companies()
+    {
+        return view('admin/companies/index');
+    }	
 }
