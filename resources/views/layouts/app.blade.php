86c55c894578a91f60eee4aa056eda43d01c79b6
muramidaza <muramidaza@gmail.com>
2019-11-29 22:04:48 +0300

    full adminka

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,6 +66,7 @@
 									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 										@csrf
 									</form>
+									<a class="dropdown-item" href="admin">Admin</a>
 								</div>
 							</li>
 						@endguest
